540a403c63d0dd0b2ed7b526df73a6075215f89a
Gavin Dreyer <gavindreyer1@gmail.com>
2020-06-02 12:10:46 -0700

    cleaning up some warning playListButton

--- src/components/audio-details/audio-details.component.jsx
+++ src/components/audio-details/audio-details.component.jsx
@@ -7,60 +7,61 @@ import { connect } from "react-redux";
 import AudioDetailsText from "./audio-details-text.component";
 
 const AudioDetails = () => {
-  const [collapse, setCollapse] = useState(false);
+	const [collapse, setCollapse] = useState(false);
 
-  const openAudioDetails = () => {
-    setCollapse(!collapse);
-  };
+	const openAudioDetails = () => {
+		setCollapse(!collapse);
+	};
 
-  return (
-    <AudioDetailsContainer className="scroll">
-      <div className="audioDiv">
-        <div
-          id="sideBarLD2"
-          href="#"
-          onClick={() => openAudioDetails()}
-          style={{
-            color: "white",
-            marginTop: 8,
-            marginRight: 75,
-            fontSize: 12
-          }}
-        >
-          What does this mean?
-        </div>
-      </div>
-      <List className="scroll">
-        <Paper
-          className={
-            collapse
-              ? "audio-details-open scroll"
-              : "audio-details-closed scroll"
-          }
-          style={{
-            maxHeight: "45vh",
-            width: 377,
-            overflow: "auto",
-            backgroundColor: "#1E2024",
-            color: "lightgray"
-          }}
-        >
-          <div className="scroll">
-            {audioDetailsText.map(({ name, description }) => (
-              <AudioDetailsText
-                name={name}
-                description={description}
-              ></AudioDetailsText>
-            ))}
-          </div>
-        </Paper>
-      </List>
-      <Grid item>
-        <Chart />
-        <div style={{ textAlign: "center", marginTop: "10px" }}></div>
-      </Grid>
-    </AudioDetailsContainer>
-  );
+	return (
+		<AudioDetailsContainer className="scroll">
+			<div className="audioDiv">
+				<div
+					id="sideBarLD2"
+					href="#"
+					onClick={() => openAudioDetails()}
+					style={{
+						color: "white",
+						marginTop: 8,
+						marginRight: 75,
+						fontSize: 12
+					}}
+				>
+					What does this mean?
+				</div>
+			</div>
+			<List className="scroll">
+				<Paper
+					className={
+						collapse
+							? "audio-details-open scroll"
+							: "audio-details-closed scroll"
+					}
+					style={{
+						maxHeight: "45vh",
+						width: 377,
+						overflow: "auto",
+						backgroundColor: "#1E2024",
+						color: "lightgray"
+					}}
+				>
+					<div className="scroll">
+						{audioDetailsText.map(({ name, description }, idx) => (
+							<AudioDetailsText
+								key={idx}
+								name={name}
+								description={description}
+							></AudioDetailsText>
+						))}
+					</div>
+				</Paper>
+			</List>
+			<Grid item>
+				<Chart />
+				<div style={{ textAlign: "center", marginTop: "10px" }}></div>
+			</Grid>
+		</AudioDetailsContainer>
+	);
 };
 
 const mapStateToProps = state => ({});
--- src/index.js
+++ src/index.js
@@ -14,12 +14,9 @@ import rootReducer from "./redux/reducers";
 
 const history = createBrowserHistory();
 let store = null;
-
+// logger,
 if (process.env.NODE_ENV === "development") {
-	store = createStore(
-		rootReducer,
-		composeWithDevTools(applyMiddleware(logger, thunk))
-	);
+	store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 } else {
 	store = createStore(rootReducer, applyMiddleware(thunk));
 }
